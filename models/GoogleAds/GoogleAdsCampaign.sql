d44bc147b4a42677fae4ca3848265ecafd279720
abijithmenon-saras <141811659+abijithmenon-saras@users.noreply.github.com>
2023-10-10 16:48:01 +0530

    Update GoogleAdsCampaign.sql

--- models/GoogleAds/GoogleAdsCampaign.sql
+++ models/GoogleAds/GoogleAdsCampaign.sql
@@ -11,7 +11,7 @@
 
 {% if is_incremental() %}
 {%- set max_loaded_query -%}
-SELECT coalesce(MAX(_daton_batch_runtime) - 2592000000,0) FROM {{ this }}
+select coalesce(max(_daton_batch_runtime) - 2592000000,0) from {{ this }}
 {% endset %}
 
 {%- set max_loaded_results = run_query(max_loaded_query) -%}
@@ -51,8 +51,12 @@ SELECT coalesce(MAX(_daton_batch_runtime) - 2592000000,0) FROM {{ this }}
             {% set store = var('default_storename') %}
         {% endif %}
 
-    SELECT * {{exclude()}} (row_num)
-    From (
+        {% if var('timezone_conversion_flag') and i.lower() in tables_lowercase_list and i in var('raw_table_timezone_offset_hours')%}
+            {% set hr = var('raw_table_timezone_offset_hours')[i] %}
+        {% else %}
+            {% set hr = 0 %}
+        {% endif %}
+
         select
         '{{brand}}' as brand,
         '{{store}}' as store,
@@ -64,42 +68,23 @@ SELECT coalesce(MAX(_daton_batch_runtime) - 2592000000,0) FROM {{ this }}
             cast(null as string) as exchange_currency_code, 
         {% endif %}
         a.* from (
-        select 
-        {% if target.type =='snowflake' %}
-        COALESCE(CUSTOMER.VALUE:currency_code::VARCHAR,'') as currency_code,
-        CAMPAIGN.VALUE:name::VARCHAR as campaign_name,
-        COALESCE(CAMPAIGN.VALUE:id::VARCHAR,'') as campaign_id,
-        CAMPAIGN.VALUE:advertising_channel_type::VARCHAR as campaign_advertising_channel_type,
-        CAMPAIGN.VALUE:advertising_channel_sub_type::VARCHAR as campaign_advertising_channel_sub_type,
-        METRICS.VALUE:clicks::NUMERIC as clicks,
-        METRICS.VALUE:conversions as conversions,
-        METRICS.VALUE:cost_micros::FLOAT as cost_micros,
-        METRICS.VALUE:conversions_value AS conversions_value,
-        METRICS.VALUE:impressions::NUMERIC as impressions,
-        SEGMENTS.VALUE:date::DATE as date,
-        {% else %}
-        customer.currency_code,
-        campaign.name as campaign_name,
-        coalesce(campaign.id,0) as campaign_id,
-        campaign.advertising_channel_type as campaign_advertising_channel_type,
-        campaign.advertising_channel_sub_type as campaign_advertising_channel_sub_type,
-        metrics.clicks,
-        metrics.conversions,
-        metrics.cost_micros,
-        metrics.conversions_value,
-        metrics.impressions,
-        segments.date,
-        {% endif %}
+        select
+        {{extract_nested_values("customer","currency_code","string")}} as currency_code
+        {{extract_nested_values("campaign","name","string")}} as campaign_name
+        {{extract_nested_values("campaign","id","string")}} as campaign_id
+        {{extract_nested_values("campaign","advertising_channel_type","string")}} as campaign_advertising_channel_type
+        {{extract_nested_values("campaign","advertising_channel_sub_type","string")}} as campaign_advertising_channel_sub_type
+        {{extract_nested_values("metrics","clicks","int")}} as clicks
+        {{extract_nested_values("metrics","conversions","numeric")}} as conversions
+        {{extract_nested_values("metrics","cost_micros","numeric")}} as cost_micros
+        {{extract_nested_values("metrics","conversions_value","numeric")}} as conversions_value
+        {{extract_nested_values("metrics","impressions","numeric")}} as impressions
+        {{extract_nested_values("segments","date","date")}} as date
         {{daton_user_id()}} as _daton_user_id,
         {{daton_batch_runtime()}} as _daton_batch_runtime,
         {{daton_batch_id()}} as _daton_batch_id,
         current_timestamp() as _last_updated,
         '{{env_var("DBT_CLOUD_RUN_ID", "manual")}}' as _run_id,
-        {% if target.type =='snowflake' %}
-            Dense_Rank() OVER (PARTITION BY SEGMENTS.VALUE:date, CAMPAIGN.VALUE:id, CAMPAIGN.VALUE:name  order by {{daton_batch_runtime()}} desc) row_num
-        {% else %}
-            Dense_Rank() OVER (PARTITION BY SEGMENTS.date,CAMPAIGN.id, CAMPAIGN.name order by {{daton_batch_runtime()}} desc) row_num
-        {% endif %}
 	    from {{i}} 
             {{unnesting("customer")}}
             {{unnesting("campaign")}}
@@ -107,15 +92,19 @@ SELECT coalesce(MAX(_daton_batch_runtime) - 2592000000,0) FROM {{ this }}
             {{unnesting("segments")}}
             {% if is_incremental() %}
             {# /* -- this filter will only be applied on an incremental run */ #}
-            WHERE {{daton_batch_runtime()}}  >= {{max_loaded}}
+            where {{daton_batch_runtime()}}  >= {{max_loaded}}
             {% endif %}
-    
+
+            qualify
+            {% if target.type =='snowflake' %}
+            dense_rank() over (partition by SEGMENTS.VALUE:date, CAMPAIGN.VALUE:id, CAMPAIGN.VALUE:name  order by {{daton_batch_runtime()}} desc) = 1
+            {% else %}
+            dense_rank() over (partition by SEGMENTS.date,CAMPAIGN.id, CAMPAIGN.name order by {{daton_batch_runtime()}} desc) = 1
+            {% endif %}
         ) a
             {% if var('currency_conversion_flag') %}
                 left join {{ref('ExchangeRates')}} c on date(a.date) = c.date and currency_code = c.to_currency_code  
             {% endif %}
-    )
-        where row_num = 1
     {% if not loop.last %} union all {% endif %}
     {% endfor %}
     
